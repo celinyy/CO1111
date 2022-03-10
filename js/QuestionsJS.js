e6363529947f0ed0ab4116389cf3ac9480554a1c
Maria <94197702+celinyy@users.noreply.github.com>
2022-03-10 19:15:17 +0200

    Adding a boilerplate HTML page
    just following the worksheet =)

--- js/QuestionsJS.js
+++ js/QuestionsJS.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -48,6 +48,12 @@ startGame = () => {
     getNewQuestion();
 }
 getNewQuestion = () => {
+
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        //END PAGE
+        //return window.location.assign()
+    }
+
     questionCounter++;//increment the question the moment the game starts for a new question.
     const questionIndex =  Math.floor(Math.random() * availableQuestions.length) //make it an integer
     currentQuestion = availableQuestions[questionIndex];
@@ -56,7 +62,24 @@ getNewQuestion = () => {
     choices.forEach( choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
-    })
+    });
+
+    availableQuestions.splice(questionIndex,1);
+
+    acceptingAnswers = true;
 };
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        getNewQuestion();
+    });
+});
 
 startGame();
